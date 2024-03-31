83c3d56f4c927d9c620966dabd3215590f03a890
Oski <115146064+oskiefitz@users.noreply.github.com>
2024-03-31 20:40:44 +0100

    Update and rename hebrew_testing to Hebrew_Parsing.py

---
+++ Python_tests/Hebrew_Parsing.py
@@ -0,0 +1,32 @@
+import re
+import requests
+
+def scrape_hebrewparse_page(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        # grammatical parsing for each word
+        content = re.findall(r'<a href="/hebrewparse\.htm" title="(.*?)">(.*?)</a>', response.text)
+        if content:
+            return '\n'.join(['{}: {}'.format(item[1], item[0]) for item in content])
+        else:
+            print("no data!")
+            return None
+    else:
+        print("nothing")
+        return None
+
+def save_to_file(text, file_path):
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(text)
+    print(f"Text saved to '{file_path}' successfully.")
+
+urls = [f"https://biblehub.com/text/genesis/12-{i}.htm" for i in range(1, 21)]
+
+for i, url in enumerate(urls, start=1):
+    # scraping inside 
+    scraped_content = scrape_hebrewparse_page(url)
+
+    if scraped_content:
+        file_name = f"hebrewparse_content_{i}.txt"
+        save_to_file(scraped_content, file_name)
+
